d0e52a7c5e33f42ca90fac2c41644adecb9d6a08
Praneeth <praneethg2001@gmail.com>
2022-10-07 15:48:11 -0700

    Minor fix

--- run_nf.sh
+++ run_nf.sh
@@ -6,4 +6,5 @@ fasta_file=${args[1]}
 metadata_file=${args[2]}
 
 # chmod +x bin/*
+echo "nextflow -C configs/nextflow.config run process_pathogen.nf -with-report ${work_dir}report/report.html -with-dag ${work_dir}report/flowchart.dot -with-timeline ${work_dir}report/timeline.html -with-trace ${work_dir}report/trace.txt --fasta_file ${fasta_file} --work_dir ${work_dir} --metadata_file ${metadata_file} ${@: 4}"
 nextflow -C configs/nextflow.config run process_pathogen.nf -with-report ${work_dir}report/report.html -with-dag ${work_dir}report/flowchart.dot -with-timeline ${work_dir}report/timeline.html -with-trace ${work_dir}report/trace.txt --fasta_file ${fasta_file} --work_dir ${work_dir} --metadata_file ${metadata_file} ${@: 4}
