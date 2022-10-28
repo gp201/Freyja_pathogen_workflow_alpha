18e4e825e280f279b2daeaea967b9f3926f8fbaa
Praneeth <praneethg2001@gmail.com>
2022-10-27 17:05:03 -0700

    Deprecate docker build

--- run_nf.sh
+++ run_nf.sh
@@ -6,5 +6,5 @@ fasta_file=${args[1]}
 metadata_file=${args[2]}
 
 chmod +x bin/*
-# nextflow -C configs/nextflow.config run process_pathogen.nf -with-report ${work_dir}report/report.html -with-timeline ${work_dir}report/timeline.html -with-trace ${work_dir}report/trace.txt --fasta_file ${fasta_file} --output_dir /raid/gp/Freyja/zika/output --metadata_file ${metadata_file} ${args[3:]}
-echo ${args[3@]}
+echo nextflow -C configs/nextflow.config run process_pathogen.nf -with-report ${work_dir}report/report.html -with-timeline ${work_dir}report/timeline.html -with-trace ${work_dir}report/trace.txt --fasta_file ${fasta_file} --output_dir /raid/gp/Freyja/zika/output --metadata_file ${metadata_file} ${args[@]:3}
+# echo ${args[3@]}
