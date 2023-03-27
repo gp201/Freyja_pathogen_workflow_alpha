2a1ff550a6c180df3a9f286a6e61911663428ec6
Praneeth <praneethg2001@gmail.com>
2023-03-27 10:03:41 +0530

    Add mamba, conda and micromamba profiles (#5)

--- run_nf.sh
+++ run_nf.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 args=($@)
-work_dir=${args[0]}
-fasta_file=${args[1]}
-metadata_file=${args[2]}
+profile=${args[0]}
+work_dir=${args[1]}
+fasta_file=${args[2]}
+metadata_file=${args[3]}
 
 # chmod +x bin/*
-nextflow -C configs/nextflow.config run process_pathogen.nf -with-report ${work_dir}report/report.html -with-timeline ${work_dir}report/timeline.html -with-trace ${work_dir}report/trace.txt --fasta_file ${fasta_file} --output_dir ${work_dir}output --metadata_file ${metadata_file} ${args[@]:3}
+nextflow -C configs/nextflow.config run process_pathogen.nf -profile ${profile} -with-report ${work_dir}report/report.html -with-timeline ${work_dir}report/timeline.html -with-trace ${work_dir}report/trace.txt --fasta_file ${fasta_file} --output_dir ${work_dir}output --metadata_file ${metadata_file} ${args[@]:3}
